5f4262bfb3317824413394eee700bf7557918f3e
安部 <abe@s2-pc-d003win.ocean.s2soft.co.jp>
2013-03-25 17:31:22 +0900

    config.php追加

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+define('IMAGE_STORE_PATH', './imgstore/');
+define('TMP_IMG_DIR', './tmpimg/');
+ 
+define('IMAGE_MAX_LENGTH', 400);
+define('MAX_FILE_SIZE', 1024 * 2000); // 300KB = 1KB/1024bytes * 300
+ 
+// error_reporting(E_ALL & ~E_NOTICE);
+ 
+// // GD
+// if (!function_exists('imagecreatetruecolor')) {
+//     echo "GDがインストールされていません！";
+//     exit;
+// }
